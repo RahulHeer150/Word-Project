f0b48c7ba1886be4b91b9fadec647a88bbb4ddde
Rahul Heer <rahulheer344@gmail.com>
2026-02-23 22:42:05 +0530

    new code chnages

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({word, meaning, sentance}) => {
   return (
-    <div>Card</div>
+    <div className='bg-[#161B22] border border-[#30363D] rounded-xl p-6 shadow-md transition-all duration-300 hover:scale-105 hover:shadow-[0_0_20px_#00FFF7] cursor-pointer'>
+
+    </div>
   )
 }
 
