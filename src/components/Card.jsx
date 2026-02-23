ac4a0ffab95d587468bd0376a5ed328d88f0f119
Rahul Heer <rahulheer344@gmail.com>
2026-02-23 22:44:01 +0530

    new code chnags

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,13 @@ import React from 'react'
 const Card = ({word, meaning, sentance}) => {
   return (
     <div className='bg-[#161B22] border border-[#30363D] rounded-xl p-6 shadow-md transition-all duration-300 hover:scale-105 hover:shadow-[0_0_20px_#00FFF7] cursor-pointer'>
+      <h2 className='text-2xl font-bold text-[#00FFF7] mb-3 tracking-wide'>
+        {word}
+      </h2>
+
+      <p className=''>
+
+      </p>
 
     </div>
   )
