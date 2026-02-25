2709263f41784344b84fda6da780c339da9e3463
Rahul Heer <rahulheer344@gmail.com>
2026-02-25 22:41:23 +0530

    new code changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Card from './components/Card'
+// import Card from './components/Card'
+import Form from './Form'
 //import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App=() =>{
+const [first, setfirst] = useState(second)
   return (
     <>
-     <Card/>
+     <Form/>
     </>
   )
 }
